46442a0b15a124ea611770b7e7b2ab2ac7c343ae
rickdeakins <rickdeakinsjr@gmail.com>
2024-02-12 01:01:44 -0500

    Import of established import/specific variables/functions.

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+//Import Questions
+import quizQuestions from './questions.js';
+
+//Quiz Specific Variables
+let currentQuestion = 0;
+let score = 0;
+let timeLeft = 60;
+
+//Functions
+function displayQuestion() {
+  }
+  
+  function checkAnswer(answer) {
+  }
+  
+  function endQuiz() {
+  }
+  
+  function startTimer() {
+  }
+  
+  function startQuiz() {
+  }
+  
+
+  //Sending the data to HTML
+  document.addEventListener("DOMContentLoaded", startQuiz);
