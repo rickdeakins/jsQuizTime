4f468e3d7075e8e521d8c82f735e1a6c6d449604
rickdeakins <rickdeakinsjr@gmail.com>
2024-02-12 14:56:56 -0500

    The questions for the quiz have been updated.

---
+++ assets/js/actions.js
@@ -0,0 +1,54 @@
+const quizQuestions = [
+    {
+        question: "What does CSS stand for in web development?",
+        options: [
+          "Colorful Style Sheets",
+          "Computational Style Sheets",
+          "Creative Style Sheets",
+          "Cascading Style Sheets",
+        ],
+        correctAnswer: "Cascading Style Sheets",
+    },
+    {
+        question: "What best describes mySql?",
+        options: [
+          "Reactive Database",
+          "Relational Database",
+          "Abstracted Database",
+          "Creative Style Sheets",
+        ],
+        correctAnswer: "Relational Database",
+    },
+    {
+        question: "Which of these are not part of the MERN stack??",
+        options: [
+          "React",
+          "jQuery",
+          "NodeJS",
+          "Express",
+        ],
+        correctAnswer: "jQuery",
+    },
+    {
+        question: "What does the abbreviatiom NPM stand for?",
+        options: [
+          "Node Property Manager",
+          "New Package Manager",
+          "Node Package Manager",
+          "New Parameter Marker",
+        ],
+        correctAnswer: "Node Package Manager",
+    },
+    {
+        question: "Where is an external script file referenced in HTML?",
+        options: [
+          "In the header",
+          "In the footer",
+          "As a variable",
+          "At the end of the <body> tags",
+        ],
+        correctAnswer: "At the end of the <body> tags",
+    }
+];
+
+
