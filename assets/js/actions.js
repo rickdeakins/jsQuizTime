f873fd7a7face6e9c3a2d1c191cfdfa3edf3f929
rickdeakins <rickdeakinsjr@gmail.com>
2024-02-12 22:35:30 -0500

    Functions added for quiz to operate. Currently timer is working. First question displays however missing any functionality beyond there.

--- assets/js/actions.js
+++ assets/js/actions.js
@@ -1,3 +1,4 @@
+//Questions for the quiz
 const quizQuestions = [
     {
         question: "What does CSS stand for in web development?",
@@ -51,4 +52,56 @@ const quizQuestions = [
     }
 ];
 
+//Logic Powering the Quiz
 
+  //Initializing the quiz
+  const startBtn = document.getElementById('start-btn');
+  const questionContainer = document.getElementById('question-container');
+  const timeLeft = document.getElementById('time-left');
+  
+  //Setting the timer, score, question and time limit to origination
+  let timer;
+  let score = 0;
+  let questionIndex = 0;
+  const timeLimit = 60; // seconds
+  
+  //Button that starts the quiz
+  startBtn.addEventListener('click', startQuiz);
+  
+  //Function that starts the quiz
+  function startQuiz() {
+      startTimer();
+      displayQuestion();
+    }
+  
+//Function that starts the timer
+function startTimer() {
+    let timeRemaining = timeLimit;
+    timeLeft.textContent = timeRemaining;
+    //Controlling the time interval 
+    timer = setInterval(() => {
+      timeRemaining--;
+      timeLeft.textContent = timeRemaining;
+     //When the timer runs out the quiz ends   
+      if (timeRemaining <= 0) {
+        endQuiz();
+      }
+    }, 1000);
+  }
+
+//Function to render the questions on the screen
+function displayQuestion() {
+const currentQuestion = quizQuestions[questionIndex];
+questionContainer.innerHTML = `
+    <h2>${currentQuestion.question}</h2>
+    <ul>
+    ${currentQuestion.options.map(options => `<li>${options}</li>`).join('')}
+    </ul>`;
+}
+
+function endQuiz() {
+    clearInterval(timer);
+    // Display final score and input for initials
+    // Handle saving initials and score
+  }
+  
